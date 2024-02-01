d0918140a2d9cd337d3fa11010accb62896c6f44
akesh <akeshstr@gmail.com>
2024-02-01 12:35:35 +0530

    added yarn.lock

--- src/components/NavigationMenuBar.jsx
+++ src/components/NavigationMenuBar.jsx
@@ -1,47 +1,52 @@
-"use client"
-import theme from '@/theme'
-import { AppBar, Box, Typography } from '@mui/material'
-import { useRouter } from 'next/navigation'
-import React from 'react'
+"use client";
+import theme from "@/theme";
+import { AppBar, Box, Typography } from "@mui/material";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const menus = [
-    {
-        id:"men",
-        name:"For Him",
-        href:"/men"
-    },
-    {
-        id:"women",
-        name:"For Her",
-        href:"/womens"
-    },
-    {
-        id:"accessories",
-        name:"Accessories",
-        href:"/accessories"
-    },
-]
+  {
+    id: "men",
+    name: "For Him",
+    href: "/men",
+  },
+  {
+    id: "women",
+    name: "For Her",
+    href: "/womens",
+  },
+  {
+    id: "accessories",
+    name: "Accessories",
+    href: "/accessories",
+  },
+];
 
 function NavigationMenuBar() {
-    const router = useRouter()
+  const router = useRouter();
   return (
     <>
- <AppBar sx={{display:"flex"}}>
-    <Box>
-        Hello
-    </Box>
-    <Box sx={{display:"flex", justifyContent:"start", alignItems:"center", columnGap:4}}>
- {
-    menus.map((menu) => (
-        <React.Fragment key={menu.id}>
-            <Typography onClick={() => router.push(`${menu.href}`)}>{menu.name}</Typography>
+      <AppBar sx={{ display: "flex" }}>
+        <Box>Hello</Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "start",
+            alignItems: "center",
+            columnGap: 4,
+          }}
+        >
+          {menus.map((menu) => (
+            <React.Fragment key={menu.id}>
+              <Typography onClick={() => router.push(`${menu.href}`)}>
+                {menu.name}
+              </Typography>
             </React.Fragment>
-    ))
- }
- </Box>
- </AppBar>
- </>
-  )
+          ))}
+        </Box>
+      </AppBar>
+    </>
+  );
 }
 
-export default NavigationMenuBar
+export default NavigationMenuBar;

ba128058678f9d087e89e73322e267ef281cefb1
akesh <akeshstr@gmail.com>
2024-02-21 21:01:21 +0530

    added images to slides

--- src/components/SwipeableDisplay.jsx
+++ src/components/SwipeableDisplay.jsx
@@ -9,6 +9,14 @@ import {
   A11y,
   EffectFade,
 } from "swiper/modules";
+import mimg1 from "../../public/images/1.jpg";
+import mimg2 from "../../public/images/2.jpg";
+import mimg3 from "../../public/images/3.jpg";
+import mimg4 from "../../public/images/4.jpg";
+import mimg5 from "../../public/images/5.jpg";
+import mimg6 from "../../public/images/6.jpg";
+import mimg7 from "../../public/images/7.jpg";
+import mimg8 from "../../public/images/8.jpg";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css/bundle";
@@ -19,39 +27,35 @@ import "swiper/css/bundle";
 const images = [
   {
     id: 1,
-    image: "http://placekitten.com/200/300",
+    image: mimg1,
   },
   {
     id: 2,
-    image: "https://picsum.photos/300/200",
+    image: mimg2,
   },
   {
     id: 3,
-    image: "http://satyr.io/200x300",
+    image: mimg3,
   },
   {
     id: 4,
-    image: "https://picsum.photos/300/200",
+    image: mimg4,
   },
   {
     id: 5,
-    image: "https://picsum.photos/300/200",
+    image: mimg5,
   },
   {
     id: 6,
-    image: "https://picsum.photos/300/200",
+    image: mimg6,
   },
   {
     id: 7,
-    image: "https://picsum.photos/300/200",
+    image: mimg7,
   },
   {
     id: 8,
-    image: "https://picsum.photos/300/200",
-  },
-  {
-    id: 9,
-    image: "https://picsum.photos/300/200",
+    image: mimg8,
   },
 ];
 
@@ -69,15 +73,31 @@ function SwipeableDisplay() {
       </Typography>
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={10}
+        spaceBetween={30}
         slidesPerView={3}
         navigation
-        centeredSlides
         pagination={{ clickable: true }}
       >
         {images.map((image) => (
-          <SwiperSlide key={image.id}>
-            <Image src={image.image} width={400} height={500} alt="display" />
+          <SwiperSlide
+            style={{
+              borderRadius: "24px",
+              border: "2px solid black",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+            key={image.id}
+          >
+            <Image
+              src={image.image}
+              width={400}
+              height={500}
+              style={{
+                objectFit: "cover",
+              }}
+              alt="display"
+            />
           </SwiperSlide>
         ))}
       </Swiper>
